72119e4047d031471302c04e942a9ee3be959a08
zafra <zafra@ualberta.ca>
2018-05-17 11:15:17 -0600

    refactor, create generic repository class

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data.Repositories
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+        where TEntity : class
+        where TContext:DbContext
+    {
+        protected readonly TContext Context;
+
+        protected GenericRepository(TContext context)
+        {
+            this.Context = context;
+        }
+
+        public void Add(TEntity model)
+        {
+            Context.Set<TEntity>().Add(model);
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await this.Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public bool HasChanges()
+        {
+            return this.Context.ChangeTracker.HasChanges();
+        }
+
+
+        public void Remove(TEntity model)
+        {
+            this.Context.Set<TEntity>().Remove(model);
+        }
+
+        public async Task SaveAsync()
+        {
+            await this.Context.SaveChangesAsync();
+        }
+    }
+}
